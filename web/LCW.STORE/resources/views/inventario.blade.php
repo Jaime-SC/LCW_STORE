1a38463365a220090b671125b6600261dda28e64
Jaime.SC <jaimeqsanchezc@gmail.com>
2021-07-29 04:24:37 -0400

    Clase 10/06/2021

--- web/LCW.STORE/resources/views/inventario.blade.php
+++ web/LCW.STORE/resources/views/inventario.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.master')
 @section('contenido')
-    <h1>Inventario</h1>
-    
+    <div class="row mt-5">
+        <div class="col-12 col-md-12 col-lg-6 mx-auto">
+            <table class="table table=hover table=bordered table=striped table-responsive">
+              <thead class="bg-gradient text-light" style="background-color: #083D77;">
+                <tr>
+                  <td>Nombre</td>
+                  <td>Producto</td>
+                  <td>Talla</td>
+                  <td>Cantidad</td>
+                </tr>
+
+              </thead>
+              <tbody id="tbody-inventario">
+
+              </tbody>
+
+            </table>
+
+        </div>
+
+    </div>
+
 @endsection
